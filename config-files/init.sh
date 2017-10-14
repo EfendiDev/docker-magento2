793bdd735b84b7ac79d5edc3bb9a74ea46522d3d
Gökhan Efendi <gokhanefendi@gmail.com>
2017-10-14 18:57:17 +0300

    Rename project directory

--- config-files/init.sh
+++ config-files/init.sh
@@ -2,19 +2,19 @@
 
 /usr/sbin/php-fpm7.0 -R &
 /usr/sbin/nginx -g "daemon off;" &
-/usr/bin/memcached -u root &
+/usr/bin/memcached -u root
 echo "Welcome on Board!"
 
-file="/data/project/docker-magento2/.magento2_installed"
+file="/data/projects/docker-magento2/.magento2_installed"
 
 if [ -f "$file" ]
 then
-	php /data/project/docker-magento2/magento2/bin/magento setup:store-config:set --base-url="http://${MAGENTO_BASE_URL}"
-	php /data/project/docker-magento2/magento2/bin/magento cache:flush
+	php /data/projects/docker-magento2/magento2/bin/magento setup:store-config:set --base-url="http://${MAGENTO_BASE_URL}"
+	php /data/projects/docker-magento2/magento2/bin/magento cache:flush
 else
-    composer create-project --repository-url=https://${MAGENTO_PRIVATE_KEY}:${MAGENTO_PUBLIC_KEY}@repo.magento.com/ magento/project-community-edition /data/project/docker-magento2/magento2
-	if php /data/project/docker-magento2/magento2/bin/magento setup:install --base-url="http://${MAGENTO_BASE_URL}/" --db-host="magento2_mysql" --db-name="magento2" --db-user="root" --admin-firstname="admin" --admin-lastname="admin" --admin-email="user@example.com" --admin-user="admin" --admin-password="Admin123*!" --language="en_US" --currency="USD" --timezone="Europe/Istanbul" --use-rewrites="1" --backend-frontname="admin" ; then
-	    echo "" > /data/project/docker-magento2/.magento2_installed
+        composer create-project --repository-url=https://${MAGENTO_PUBLIC_KEY}:${MAGENTO_PRIVATE_KEY}@repo.magento.com/ magento/project-community-edition /data/projects/docker-magento2/magento2
+	if php /data/projects/docker-magento2/magento2/bin/magento setup:install --base-url="http://${MAGENTO_BASE_URL}/" --db-host="magento2_mysql" --db-name="magento2" --db-user="root" --admin-firstname="admin" --admin-lastname="admin" --admin-email="user@example.com" --admin-user="admin" --admin-password="Admin123*!" --language="en_US" --currency="USD" --timezone="Europe/Istanbul" --use-rewrites="1" --backend-frontname="admin" ; then
+	    echo "" > /data/projects/docker-magento2/.magento2_installed
 	    echo "Magento 2 Community Edition successfully installed."
 	else
 	    echo "Oops, I need help! ¯\_(ツ)_/¯ "

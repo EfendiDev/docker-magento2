a1c322878ffa1f6748aab08b2a7658596ed7bded
Gökhan Efendi <gokhanefendi@gmail.com>
2017-10-22 18:26:44 +0300

    Update init.sh

--- config-files/init.sh
+++ config-files/init.sh
@@ -19,7 +19,7 @@ then
 	php /data/project/magento2/bin/magento cache:flush
 else
     composer create-project --repository-url=https://${MAGENTO_PRIVATE_KEY}:${MAGENTO_PUBLIC_KEY}@repo.magento.com/ magento/project-community-edition /data/project/mageshopfinder/magento2
-    if php /data/project/mageshopfinder/magento2/bin/magento setup:install --base-url="http://${MAGENTO_BASE_URL}/" --db-host="magento2_mysql" --db-name="magento2" --db-user="root" --admin-firstname="admin" --admin-lastname="admin" --admin-email="user@example.com" --admin-user="admin" --admin-password="Admin123*!" --language="en_US" --currency="USD" --timezone="Europe/Istanbul" --use-rewrites="1" --backend-frontname="admin" ; then		  	if php /data/project/mageshopfinder/magento2/bin/magento setup:install --base-url="http://${MAGENTO_BASE_URL}/" --db-host="magento2_mysql" --db-name="magento2" --db-user="root" --admin-firstname="admin" --admin-lastname="admin" --admin-email="user@example.com" --admin-user="admin" --admin-password="Admin123*!" --language="en_US" --currency="USD" --timezone="Europe/Istanbul" --use-rewrites="1" --backend-frontname="admin" ; then
+    if php /data/project/mageshopfinder/magento2/bin/magento setup:install --base-url="http://${MAGENTO_BASE_URL}/" --db-host="magento2_mysql" --db-name="magento2" --db-user="root" --admin-firstname="admin" --admin-lastname="admin" --admin-email="user@example.com" --admin-user="admin" --admin-password="admin123" --language="en_US" --currency="USD" --timezone="Europe/Istanbul" --use-rewrites="1" --backend-frontname="admin" ; then
   	    echo "" > /data/project/mageshopfinder/.magento2_installed
             echo "Magento 2 Community Edition successfully installed."
             cd /data/project/magento2

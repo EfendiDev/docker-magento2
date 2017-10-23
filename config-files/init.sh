2fcf5dcc989bb9f85e619380ed78b0cc138ab049
Gökhan Efendi <gokhanefendi@gmail.com>
2017-10-23 09:19:01 +0300

    Fix Unix End-of-line Issue

--- config-files/init.sh
+++ config-files/init.sh
@@ -4,29 +4,29 @@ echo "Welcome on Board!"
 
 if [ "$MAGENTO_BASE_URL" = "" ]
 then
-	echo "MAGENTO_BASE_URL is missing!"
-	echo "Please set it using this command:"
-	echo "export MAGENTO_BASE_URL=http://magento2.local"
-	echo "Then re-try it please!"
-	exit
+    echo "MAGENTO_BASE_URL is missing!"
+    echo "Please set it using this command:"
+    echo "export MAGENTO_BASE_URL=http://magento2.local"
+    echo "Then re-try it please!"
+    exit
 fi
 
 file="/data/project/.magento2_installed"
 
 if [ -f "$file" ]
 then
-	php /data/project/magento2/bin/magento setup:store-config:set --base-url="${MAGENTO_BASE_URL}"
-	php /data/project/magento2/bin/magento cache:flush
+    php /data/project/magento2/bin/magento setup:store-config:set --base-url="${MAGENTO_BASE_URL}"
+    php /data/project/magento2/bin/magento cache:flush
 else
-    composer create-project --repository-url=https://${MAGENTO_PRIVATE_KEY}:${MAGENTO_PUBLIC_KEY}@repo.magento.com/ magento/project-community-edition /data/project/mageshopfinder/magento2
-    if php /data/project/mageshopfinder/magento2/bin/magento setup:install --base-url="http://${MAGENTO_BASE_URL}/" --db-host="magento2_mysql" --db-name="magento2" --db-user="root" --admin-firstname="admin" --admin-lastname="admin" --admin-email="user@example.com" --admin-user="admin" --admin-password="admin123" --language="en_US" --currency="USD" --timezone="Europe/Istanbul" --use-rewrites="1" --backend-frontname="admin" ; then
-  	    echo "" > /data/project/mageshopfinder/.magento2_installed
-            echo "Magento 2 Community Edition successfully installed."
-            cd /data/project/magento2
-            composer require efendi/shopfinder dev-master
-            composer install
-            php /data/project/magento2/bin/magento setup:upgrade
-            php /data/project/magento2/bin/magento cache:flus
+    composer create-project --repository-url=https://${MAGENTO_PRIVATE_KEY}:${MAGENTO_PUBLIC_KEY}@repo.magento.com/ magento/project-community-edition /data/project/magento2
+    if php /data/project/magento2/bin/magento setup:install --base-url="${MAGENTO_BASE_URL}" --db-host="magento2_mysql" --db-name="magento2" --db-user="root" --admin-firstname="admin" --admin-lastname="admin" --admin-email="user@example.com" --admin-user="admin" --admin-password="admin123" --language="en_US" --currency="USD" --timezone="Europe/Istanbul" --use-rewrites="1" --backend-frontname="admin" ; then
+        echo "" > /data/project/.magento2_installed
+        echo "Magento 2 Community Edition successfully installed."
+        cd /data/project/magento2
+        composer require efendi/shopfinder dev-master
+        composer install
+        php /data/project/magento2/bin/magento setup:upgrade
+        php /data/project/magento2/bin/magento cache:flus
    fi
 fi
 
